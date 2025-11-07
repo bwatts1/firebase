9c27b321a4a4d2c589c1c2e849d96e5be391448f
bwatts1 <wattsbraydon2002@gmail.com>
2025-11-07 16:48:32 -0500

    adjusted product to include category and quantity

--- lib/services/product.dart
+++ lib/services/product.dart
@@ -2,17 +2,23 @@ class Product {
   String id;
   String name;
   double price;
+  String category; 
+  double quantity; 
 
   Product({
     required this.id,
     required this.name,
     required this.price,
+    required this.category,
+    required this.quantity,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'name': name,
       'price': price,
+      'quantity': quantity,
+      'category': category,
     };
   }
 
@@ -21,6 +27,8 @@ class Product {
       id: id,
       name: data['name'] ?? '',
       price: (data['price'] ?? 0).toDouble(),
+      category: data['category'] ?? 'misc',
+      quantity: (data['quantity'] ?? 1).toDouble(),
     );
   }
 }
